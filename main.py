a7d4d8085b27bb2a8f6108b1c1c5889ed79b9170
robert <robert.gontaru@outlook.com>
2023-01-24 22:31:55 +0000

    sd

--- main.py
+++ main.py
@@ -1,60 +1,113 @@
+# Pygame
 import pygame
-
-# Set up the pygame window
 pygame.init()
-window_size = (400, 400)
-screen = pygame.display.set_mode(window_size)
 
-# Set up the colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
+# Look
+size = (900, 500)
+screen = pygame.display.set_mode(size)
+fps = 60
+clock = pygame.time.Clock()
+background_image = pygame.image.load("background.png").convert()
 
-# Set up the board
-board = []
-for i in range(8):
-    board.append([])
-    for j in range(8):
-        if (i + j) % 2 == 0:
-            board[i].append(RED)
-        else:
-            board[i].append(GREEN)
+# Variables
+w_pressed = False
+s_pressed = False
+left_pressed = False
+right_pressed = False
+player_one = pygame.Rect(size[0]/2 - 25, size[1] - 25, 50, 50)
+player_two = pygame.Rect(size[0] - 60, size[1]/1 - 100, 50, 50)
+gravity = 0.7
+# Y axis velocity (Player 1)
+vy = 0
+# X axos velocity (Player 2)
+vx = 0
 
-# Set up the checkers
-checkers = []
-for i in range(3):
-    for j in range(8):
-        if (i + j) % 2 == 1:
-            checkers.append((i, j, WHITE))
+# Top Boundaries Variables
+top_boundary = 0
+bottom_boundary = size[1] - player_one.height
+on_bottom_boundry = True
 
-for i in range(5, 8):
-    for j in range(8):
-        if (i + j) % 2 == 1:
-            checkers.append((i, j, BLACK))
+# Side Boundaries Variables
+left_boundary = 25
+right_boundary = size[0] - 25
+on_left_boundry = False
+on_right_boundry = True
 
-# Set up the game state
-selected_checker = None
-current_player = BLACK
-game_over = False
-
-# Main game loop
-while not game_over:
-    # Handle events
+# Main
+running = True
+while running:
+    # Events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            game_over = True
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_x, mouse_y = event.pos
-            column = mouse_x // 50
-            row = mouse_y // 50
-            checker_index = None
-            for i, (r, c, color) in enumerate(checkers):
-                if r == row and c == column:
-                    checker_index = i
-                    break
-            if checker_index is not None:
-                if checkers[checker_index][2] == current_player:
-                    selected_checker = checker_index
-                else:
-                    selected_checker = None
+            running = False
+    keys = pygame.key.get_pressed()
+
+    # W controller
+    if keys[pygame.K_w]:
+        if not w_pressed:
+            w_pressed = True
+            if on_bottom_boundry:
+                    vy = -15
+    else:
+        w_pressed = False
+    # S Controller
+    if keys[pygame.K_s]:
+        s_pressed = True
+    else:
+        s_pressed = False
+    # Right Controller
+    if keys[pygame.K_RIGHT]:
+        if not right_pressed:
+            right_pressed = True
+            if on_left_boundry:
+                vx = 15
+    else:
+        right_pressed = False
+    # Left Controller
+    if keys[pygame.K_LEFT]:
+        if not left_pressed:
+            left_pressed = True
+            if on_right_boundry:
+                vx = -15
+    else:
+        left_pressed = False
+    # Jumping/Falling
+    if s_pressed:
+        vy += gravity * 4
+    else:
+        vy += gravity
+    player_one.y += vy
+    # Left/Right
+    player_two.x += vx
+    
+    # Boundries P1
+    if player_one.top <= top_boundary:
+        player_one.top = top_boundary
+        vy = 0
+    if player_one.bottom >= bottom_boundary:
+        player_one.bottom = bottom_boundary
+        vy = 0
+        on_bottom_boundry = True
+    else:
+        on_bottom_boundry = False
+    # Boundries P2
+    if player_two.right >= right_boundary:
+        player_two.right = right_boundary
+        vx = 0
+        on_right_boundry = True
+    else:
+        on_right_boundry = False
+    if player_two.left <= left_boundary:
+        player_two.left = left_boundary
+        vx = 0
+        on_left_boundry = True
+    else:
+        on_left_boundry = False
+    # Final additions
+
+    screen.blit(background_image, [0, 0])
+    pygame.draw.rect(screen, (255, 0, 0), player_one)
+    pygame.draw.rect(screen, (0, 0, 255), player_two)
+    pygame.display.flip()
+    clock.tick(fps)
+pygame.quit()

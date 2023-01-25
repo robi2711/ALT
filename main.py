f1be79ecf36f4a72372cf6331a9e00475deeec19
robert <50277245+robi2711@users.noreply.github.com>
2023-01-25 11:39:14 +0000

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,10 @@ w_pressed = False
 s_pressed = False
 left_pressed = False
 right_pressed = False
-player_one = pygame.Rect(size[0]/2 - 25, size[1] - 25, 50, 50)
+player_one = pygame.Rect(size[0]/2 - 30, size[1] - 25, 50, 50)
 player_two = pygame.Rect(size[0] - 60, size[1]/1 - 100, 50, 50)
 gravity = 0.7
+p1_right_pos = player_one.right
 # Y axis velocity (Player 1)
 vy = 0
 # X axos velocity (Player 2)
@@ -25,13 +26,13 @@ vx = 0
 # Top Boundaries Variables
 top_boundary = 0
 bottom_boundary = size[1] - player_one.height
-on_bottom_boundry = True
+on_bottom_boundary = True
 
 # Side Boundaries Variables
 left_boundary = 25
 right_boundary = size[0] - 25
-on_left_boundry = False
-on_right_boundry = True
+on_left_boundary = False
+on_right_boundary = True
 
 # Main
 running = True
@@ -46,10 +47,12 @@ while running:
     if keys[pygame.K_w]:
         if not w_pressed:
             w_pressed = True
-            if on_bottom_boundry:
-                    vy = -15
+            if on_bottom_boundary:
+                vy = -15
+                p1_rightpos = 2000
     else:
         w_pressed = False
+        p1_rightpos = player_one.right
     # S Controller
     if keys[pygame.K_s]:
         s_pressed = True
@@ -59,7 +62,7 @@ while running:
     if keys[pygame.K_RIGHT]:
         if not right_pressed:
             right_pressed = True
-            if on_left_boundry:
+            if on_left_boundary:
                 vx = 15
     else:
         right_pressed = False
@@ -67,7 +70,7 @@ while running:
     if keys[pygame.K_LEFT]:
         if not left_pressed:
             left_pressed = True
-            if on_right_boundry:
+            if on_right_boundary:
                 vx = -15
     else:
         left_pressed = False
@@ -87,22 +90,27 @@ while running:
     if player_one.bottom >= bottom_boundary:
         player_one.bottom = bottom_boundary
         vy = 0
-        on_bottom_boundry = True
+        on_bottom_boundary = True
     else:
-        on_bottom_boundry = False
+        on_bottom_boundary = False
     # Boundries P2
     if player_two.right >= right_boundary:
         player_two.right = right_boundary
         vx = 0
-        on_right_boundry = True
+        on_right_boundary = True
     else:
-        on_right_boundry = False
+        on_right_boundary = False
     if player_two.left <= left_boundary:
         player_two.left = left_boundary
         vx = 0
-        on_left_boundry = True
+        on_left_boundary = True
     else:
-        on_left_boundry = False
+        on_left_boundary = False
+    # Kill Boxes
+    if player_two.right == p1_rightpos:
+        print("L")
+    print(p1_rightpos)
+    print(player_two.right)
     # Final additions
 
     screen.blit(background_image, [0, 0])
